cd56bd5662158edbb7a41a5cbf816b93fd57d38c
Guilherme <guimaraes_gui@outlook.com.br>
2023-11-08 14:15:45 -0300

    Add tipos de variaveis

---
+++ variaveis.py
@@ -0,0 +1,36 @@
+# > VARIÁVEIS
+
+# 1. Variáveis
+
+idade = 22 # Criando uma variável
+
+print(idade) # Imprimindo uma variável
+
+nome = 'Guilherme Guimarães'
+
+print(nome)
+
+""" 
+    Tipos de variáveis:
+    1. int: números inteiros.
+    2. float: números reais.
+    3. str: cadeias de caracteres (textos/string).
+    4. bool: valores lógicos (booleanos): True ou False. O.B.S: Inicial sempre maiúscula.
+"""
+
+# Exemplos de variaveis
+idade = 22
+altura = 1.77
+nome = "Guilherme Guimarães"
+estudando = True
+
+print(type(idade))
+print(type(altura))
+print(type(nome))
+print(type(estudando))
+
+# Obtendo dados do usuário e salvando em variáveis
+
+linguagem = input("Qual é a linguagem de programação que você está estudando? ")
+
+print("A linguagem que você está estudando é: ", linguagem)
