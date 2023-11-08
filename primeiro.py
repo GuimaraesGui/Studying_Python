6863b98787220149c34cd7f6816ecd95e61af416
Guilherme <guimaraes_gui@outlook.com.br>
2023-11-08 14:46:32 -0300

    .\primeiro.py

--- primeiro.py
+++ primeiro.py
@@ -4,3 +4,7 @@ print('Hello World!')
 print("Seja bem-vindo(a) ao curso de Python do Coding Tank!")
 
 input("Qual é a linguagem de programação que você está estudando? ")
+
+print(type) #Imprime o tipo da variavel
+
+# Variável = input("Texto") -> A variável recebe o valor lido.
