dea1ccaf7c881cb537df11cb80906ad792c708eb
Guilherme G. <ep.0.season.@gmail.com>
2024-01-24 15:08:04 -0300

    Base de contatos para CFQ

---
+++ E-mail_automation/contatos_cfq.py
@@ -0,0 +1,33 @@
+#Enviar somente CFQ
+contatos_Postos_gnv = ['claudiomanuel900@gmail.com','itu@postos7.com.br','ocruz@sinergas.com.br','juliobertolai@hotmail.com','tatiana.ponsoni@gpabr.com',
+              'apvalinho@redecomendador.com.br','postocompanheiro@uol.com.br','kimdog@uol.com.br','mlucca@combustiveisblue.com.br','fernandafantinato@gmail.com',
+              'suportefantinato@globo.com','postobelavista@costaebarros.com.br','postosanfrancisco@gmail.com','renan_bonetto@yahoo.com.br','elsantos@naturgy.com',
+              'dcanossa@aspro.com.br','rsoro@ipem.sp.gov.br','jhfazano@terra.com.br','autopostoipanema@terra.com.br','a4posto@gmail.com','renan_bonetto@yahoo.com.br',
+              'miqueias@redecomendador.com.br','tatiana.ponsoni@multivarejogpa.com.br','rh@costaebarros.com.br','postotivoli@terra.com.br','autopostotivoli@gmail.com',
+              'bruno.vilela@vibraenergia.com.br','feikes@vibraenergia.com.br','autopostoboavista2016@gmail.com','itu@postos7.com.br','claudiomanuel900@gmail.com',
+              'mlucca@combustiveisblue.com.br']
+
+contatos_Auto_posto_Bela_Vista = ['postobelavista@costaebarros.com.br']
+
+#Enviar consumo diário + CFQ
+contatos_ajinomoto = ['sarah_carvalho@br.ajinomoto.com', 'edson_zanetti@br.ajinomoto.com', 'vinicius_leal@br.ajinomoto.com']
+
+contatos_De_Nora = ['Paulo.Gusmao@denora.com','antonino.harada@denora.com','fabio.silva@denora.com',
+                    'helio.oliveirajr@denora.com', 'Felipe.Silva@denora.com','bruno.darros@denora.com']
+
+contatos_CBA_Metalex = ['gabriela.freitas@metalex.com.br', 'anderson.duck@cba.com.br', 'mauricio.carriel@cba.com.br', 'natalia.antiqueira@cba.com.br',
+                        'coordenacao.energia@cba.com.br', 'cledson.correa@cba.com.br', 'tony.souza.ts1@cba.com.br', 'saniel.romani.sr1@cba.com.br',
+                        'felippe.sanvezzo.fs1@cba.com.br', 'cid.souza@cba.com.br', 'reginaldo.jose@cba.com.br', 'fabricio.tiseo@cba.com.br', 
+                        'manoel.reis@cba.com.br', 'carlos.cavalcante@cba.com.br', 'luiz.santinellisarti@cba.com.br', 'alan.silva.as3@metalex.com.br',
+                        'luiz.gomes.lg2@cba.com.br', 'vitor.bezerra@cba.com.br', 'laurin.santos.ls1@cba.com.br', 'natalia.antiqueira@cba.com.br',
+                        'milena.souza@cba.com.br', 'wellington.padilha@cba.com.br', 'gabriela.silveira@cba.com.br', 'eric.santos@cba.com.br',
+                        'gustavo.alves@cba.com.br', 'andre.arantes@cba.com.br', 'felippe.sanvezzo.fs1@cba.com.br', 'mauro.canal.mc1@cba.com.br',
+                        'Processo-RefinariadeAlumina@cba.com.br', 'pablo.richtchik@cba.com.br', 'rubens.costa.rc1@cba.com.br', 'alan.silva.as3@metalex.com.br',
+                        'larissa.diniz@cba.com.br']
+
+contatos_Sanovo = ['adriano.canton@sanovo.com.br']
+
+contatos_symrise = ['gabriel.camargo@symrise.com', 'marcelo.campos@symrise.com', 'jean.silvestre@symrise.com', 'Arthur.Arruda@symrise.com']
+
+contatos_toyota = ['rsimonelli@toyota.com.br']
+                   
